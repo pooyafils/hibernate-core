e3cc23ae8b6ea288ab426eacd66293b0622af03b
pooyafils <pooyafils@gmail.com>
2023-07-23 09:13:18 +0330

    many to many create

---
+++ src/main/java/com/relationaltables/manymany/AddCourseForStudent.java
@@ -0,0 +1,2 @@
+package com.relationaltables.manymany;public class AddCourseForStudent {
+}
---
+++ src/main/java/com/relationaltables/manymany/Review.java
@@ -0,0 +1,35 @@
+package com.relationaltables.manyone.UniDirectinal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String comment;
+
+    public Review() {
+    }
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "comment='" + comment + '\'' +
+                '}';
+    }
+}
